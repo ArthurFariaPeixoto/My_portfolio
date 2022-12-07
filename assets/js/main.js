f163239bca674b2936cc0ad4e1302f3a44e932e2
arthu <arthurfpeixoto@gmail.com>
2022-12-06 22:19:16 -0300

    Insert night mode and some features

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,3 +91,80 @@ modalCloses.forEach((modalClose) =>{
         })
     })
 })
+
+
+/*Deixando o nav menu claro para o usuario*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop-50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop +sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+/*Alterar background do header*/
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    if(this.scrollY >=80){
+        nav.classList.add('scroll-header')
+    }else{
+        nav.classList.remove('scroll-header')
+    }
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+/*Exibe scroll para cima*/
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-up')
+    if(this.scrollY >= 560){
+        scrollTop.classList.add('show-scroll')
+    }else{
+        scrollTop.classList.remove('show-scroll')
+    }
+}
+window.addEventListener('scroll', scrollTop)
+
+/*ATIVA MODO ESCURO*/
+const themeButton = document.getElementById('theme_button')
+const darkTheme = 'dark_theme'
+const iconTheme = 'fa-sun-o'
+
+
+const selectedTheme = localStorage.getItem('selected_theme')
+const selectedIcon = localStorage.getItem('selected_icon')
+
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'fa-moon-o' : 'fa-sun-o'
+
+
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'fa-moon-o' ? 'add' : 'remove'](iconTheme)
+
+}
+
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected_theme', getCurrentTheme())
+    localStorage.setItem('selected_icon', getCurrentIcon())
+})
